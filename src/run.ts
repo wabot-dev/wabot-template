2d3944b8748fa221b0a8ae19311e9ffc5957fea9
Jorge Narvaez <jormelcn@gmail.com>
2025-04-26 16:36:45 -0500

    feat: update project dependencies and configuration, enhance run server setup

--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,22 @@
-import { runServer } from '@wabot-dev/framework'
+import {
+  ChatBotAdapter,
+  ChatRepository,
+  OpenaiChatBotAdapter,
+  RamChatRepository,
+  runServer,
+} from '@wabot-dev/framework'
 import { MyController } from './controllers/MyController'
 
 runServer({
   controllers: [MyController],
+  providers: [
+    {
+      replace: ChatRepository,
+      with: RamChatRepository,
+    },
+    {
+      replace: ChatBotAdapter,
+      with: OpenaiChatBotAdapter,
+    },
+  ],
 })
