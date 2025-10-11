6ba8dbdc945d8fdf1117ae0d9f2c0e200dc18c91
Ricardober93 <ribermudez@petalmail.com>
2025-10-10 22:37:54 -0500

    feat: update implementations., version

--- src/mindsets/MyMindset.ts
+++ src/mindsets/MyMindset.ts
@@ -1,7 +1,20 @@
-import { mindset, type IMindset, type IMindsetIdentity } from '@wabot-dev/framework'
+import { IMindsetLlm, mindset, type IMindset, type IMindsetIdentity } from '@wabot-dev/framework'
 
 @mindset()
 export class MyMindset implements IMindset {
+  async workflow(): Promise<string> {
+    return ``
+  }
+
+  async llms(): Promise<IMindsetLlm[]> {
+    return [
+      {
+        model: 'gpt-4o',
+        provider: 'openai',
+      },
+    ]
+  }
+
   async identity(): Promise<IMindsetIdentity> {
     return {
       name: 'Elia',
